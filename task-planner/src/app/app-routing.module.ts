bc46b3e9f866823b151146b8b2900e51f2928290
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-11-02 21:10:34 +0300

    2. Маршрутизация. Основные элементы.[#381344]

--- task-planner/src/app/app-routing.module.ts
+++ task-planner/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TaskListComponent} from './task/tasks/task-list/task-list.component';
+import {NotesListComponent} from './task/tasks/notes-list/notes-list.component';
+import {BrowserModule} from '@angular/platform-browser';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'tasks', pathMatch: 'full'},
+  {path: 'tasks', component: TaskListComponent},
+  {path: 'notes', component: NotesListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- task-planner/src/app/task/task.module.ts
+++ task-planner/src/app/task/task.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NotesListComponent } from './tasks/notes-list/notes-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotesListComponent],
   imports: [
     CommonModule,
     FormsModule,
