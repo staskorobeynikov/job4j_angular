297af704794c30286c16779aada593b9ecad5e41
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-11-19 19:12:36 +0300

    1. Создание формы [#381355]

--- task-planner/src/app/registration/registration.component.ts
+++ task-planner/src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,13 +7,25 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+  registrationForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
-  submitForm(loginForm: NgForm) {
-    console.log('submit', loginForm);
+  buildForm() {
+    this.registrationForm = new FormGroup({
+      email: new FormControl(''),
+      lastName: new FormControl(''),
+      password: new FormControl(''),
+      phone: new FormControl(''),
+      inputState: new FormControl(''),
+      remember: new FormControl(false)
+    });
+  }
+
+  send() {
+    console.log(this.registrationForm);
   }
 }
