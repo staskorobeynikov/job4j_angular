c28d8de1b4ff7cc4cf070489c292123a92a55c14
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-01-04 19:42:45 +0300

    Директивы[#1]

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,39 +1,10 @@
-import {Component, OnInit} from '@angular/core'
-
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
-    {title: 'Следующий блок', text: 'Будет про директивы и про пайпы', id: 2}
-  ]
-
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log('Timeout')
-    //   this.posts[0] = {
-    //     title: 'Changed!!',
-    //     text: 'Changed222',
-    //     id: 33
-    //   }
-    // }, 5000)
-  }
-
-  updatePosts(post: Post): void {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number): void {
-    console.log('Id to remove', id)
-    this.posts = this.posts.filter(p => p.id !== id)
-  }
+export class AppComponent {
+  isVisible = true
 }
