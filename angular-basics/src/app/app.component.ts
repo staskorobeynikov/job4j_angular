4eb1f1a24cc8f7d0858cf61ce69b8ad84be952a3
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-01-05 22:15:06 +0300

    Создание своей ngModel[#1]

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core'
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms"
-import {MyValidators} from "./my.validators";
+import {Component} from '@angular/core'
 
 export interface Post {
   title: string
@@ -12,52 +10,6 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  form: FormGroup
-
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required,
-        MyValidators.restrictedEmails
-      ], MyValidators.uniqueEmail),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      address: new FormGroup({
-        country: new FormControl('by'),
-        city: new FormControl('Минск', Validators.required)
-      }),
-      skills: new FormArray([])
-    })
-  }
-
-  submit(): void {
-    console.log('Form: ', this.form)
-    const formData = {...this.form.value}
-
-    console.log('Form Data: ', formData)
-    this.form.reset()
-  }
-
-  setCapital(): void {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск'
-    }
-    const cityKey = this.form.get('address').get('country').value
-    const city = cityMap[cityKey]
-
-    this.form.patchValue({
-      address: {city}
-    })
-  }
-
-  addSkill(): void {
-    const control = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(control)
-  }
+export class AppComponent {
+  appState = 'on'
 }
