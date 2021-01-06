e472241fbbd1e2829150ab1f1fcb73cf6cad19e0
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-01-06 21:59:45 +0300

    Работа с сервером (HttpClient)[#1]

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,15 +1,63 @@
-import {Component} from '@angular/core'
-
-export interface Post {
-  title: string
-  text: string
-}
+import {Component, OnInit} from '@angular/core'
+import {Todo, TodosService} from "./todos.service"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  appState = 'on'
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
+
+  loading = false
+
+  todoTitle = ''
+
+  error = ''
+
+  constructor(private todosService: TodosService) {
+  }
+
+  ngOnInit(): void {
+    this.fetchTodos()
+  }
+
+  addTodo(): void {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }).subscribe(todo => {
+      this.todos.push(todo)
+      this.todoTitle = ''
+    })
+  }
+
+  fetchTodos(): void {
+    this.loading = true
+    this.todosService.fetchTodos()
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      }, error => {
+        this.error = error.message
+      })
+  }
+
+  removeTodo(id: number): void {
+    this.todosService.removeTodo(id)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id)
+      })
+  }
+
+  completeTodo(id: number): void {
+    this.todosService.completeTodo(id).subscribe(todo => {
+      this.todos.find(t => t.id === todo.id).completed = true
+    })
+  }
 }
--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import {NgModule, Provider} from '@angular/core'
 
 import { AppComponent } from './app.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import { SwitchComponent } from './switch/switch.component'
+import {FormsModule} from '@angular/forms'
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http"
+import {AuthInterceptor} from "./auth.interceptor"
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptor,
+  multi: true
+}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SwitchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
