50d3d9340b5f4b12c3d3ec3c9debdb853fd6beb0
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-01-09 19:14:00 +0300

    Как работают модули[#1]

--- angular-basics/src/app/app-routing.module.ts
+++ angular-basics/src/app/app-routing.module.ts
@@ -1,38 +1,17 @@
-import {NgModule} from "@angular/core"
-import {RouterModule, Routes} from "@angular/router"
-import {HomeComponent} from "./home/home.component"
-import {AboutComponent} from "./about/about.component"
-import {PostsComponent} from "./posts/posts.component"
-import {PostComponent} from "./post/post.component"
-import {AboutExtraComponent} from "./about-extra/about-extra.component"
-import {ErrorPageComponent} from "./error-page/error-page.component"
-import {AuthGuard} from "./auth.guard"
-import {PostResolver} from "./post.resolver"
-
-// http://localhost:4200/ -> HomeComponent
-// http://localhost:4200/about -> AboutComponent
-// http://localhost:4200/posts -> PostsComponent
-// http://localhost:4200/about/extra -> AboutExtraComponent
+import {NgModule} from '@angular/core'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
+import {HomePageComponent} from './home-page/home-page.component'
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent, canActivateChild: [AuthGuard], children: [
-      {path: 'extra', component: AboutExtraComponent}
-    ]},
-  {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
-  {
-    path: 'posts/:id',
-    component: PostComponent,
-    resolve: {
-      post: PostResolver
-    }
-  },
-  {path: 'error', component: ErrorPageComponent},
-  {path: '**', redirectTo: 'error'}
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
